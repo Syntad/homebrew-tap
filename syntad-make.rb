580c0ee0663f072c680975f0bf40834f5f62a7e7
Antfroze <realantfroze@gmail.com>
2023-07-04 20:13:29 -0500

    Update smake sha256

--- syntad-make.rb
+++ syntad-make.rb
@@ -2,7 +2,7 @@ class SyntadMake < Formula
     desc "Smake is an ultra fast build tool written in C++ using Lua as the scripting language. It includes plugin support, super easy configuration, and so much more. 📦"
     homepage "https://github.com/Syntad/smake/"
     url "https://github.com/Antfroze/smake/releases/download/v0.1.0/smake.tar.gz"
-    sha256 "fe880062e333bd2b638329f2108202e8338178b359bf95b2ab4aedb8b070a726"
+    sha256 "0900af0f4b42652a7bedde44751fc80f18af3619eeb461208d2c0ced80e84a99"
     license "MIT"
   
     def install
